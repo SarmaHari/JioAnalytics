eb38ee1776835ebba9d87d4924dcc6a92932db4f
SarmaHari <59437575+SarmaHari@users.noreply.github.com>
2020-06-30 10:34:26 +0530

    Add files via upload
    Added memory related (MI1) data code

--- Process_crash_data_V2.py
+++ Process_crash_data_V2.py
@@ -1,3 +1,4 @@
+#29-Jun-2020 : Insert data of MI1 (Device Memory) inputs & outputs will have _V2_8
 #12-Jun-2020: Fixed Time issue (convert to GMT and insert) AND Json string leading blank removed
 #10-Jun2020: Subtracting 5.30 hours and inserting into ES
 #4-Jun-2020: Crash Info /Stack splitting into various columns
@@ -16,7 +17,7 @@ def listToString(s):
     # return string   
     return str1  
 
-def Read_Five_Column_File(file_name): #alternately use Pandas reading, when reading gets complicated with more columns or selection is required etc
+def Read_Six_Column_File(file_name): #alternately use Pandas reading, when reading gets complicated with more columns or selection is required etc
     with open(file_name, 'r', encoding='utf-8') as f_input:
         csv_input = csv.reader(f_input, delimiter=',', skipinitialspace=True)
         x = []
@@ -24,6 +25,7 @@ def Read_Five_Column_File(file_name): #alternately use Pandas reading, when read
         p = []
         q = []
         r = []
+        s = []
         i = 0
 
         for cols in csv_input:
@@ -32,10 +34,11 @@ def Read_Five_Column_File(file_name): #alternately use Pandas reading, when read
             p.append(cols[2])
             q.append(cols[3])
             r.append(cols[4])
+            s.append(cols[5])
             i = i + 1
         f_input.close()
 
-    return x, y, p, q, r
+    return x, y, p, q, r, s
     
 def process_anr_info(input_data):
     anr_process_name = 'NA'
@@ -111,7 +114,9 @@ def createindex_stb_crash_anr_v2(es):
      "STBID" :{"type" :"keyword"},
      #"EVENTDATE_TIME" : {"type": "date", "format": "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis" },
      #"EVENTDATE_TIME" : {"type": "date", "format": "DD-MM-YYYY HH:mm:ss" },
-     "EVENTDATE_TIME" : {"type": "date", "format" : "dd-MM-yyyy HH:mm:ss||yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis" },
+     #"EVENTDATE_TIME" : {"type": "date", "format" : "dd-MM-yyyy HH:mm:ss||yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis" }, #This was working
+     #"EVENTDATE_TIME" : {"type": "date", "format" : "dd-MM-yyyy HH:mm:ss||yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis" },
+     "EVENTDATE_TIME" : {"type": "date", "format" : "dd-MM-yyyy HH:mm:ss" },
      "EVENT" : {"type" : "keyword"},
      "APP_VER" : {"type" : "keyword"},
      "EVENT_TYPE" : {"type" : "keyword"},
@@ -124,7 +129,9 @@ def createindex_stb_crash_anr_v2(es):
      "PACKAGE" :{"type" : "keyword"},
      "FOREGROUND" :{"type" : "keyword"},
      "SUBJECT" :{"type" : "keyword"},
-     "BUILD" :{"type" : "keyword"}
+     "BUILD" :{"type" : "keyword"},
+     "DEV_TOT_MEM" : {"type" : "float"},
+     "DEV_FREE_MEM" : {"type" : "float"}
       }
     }
     }# Ends Index Settings
@@ -132,7 +139,7 @@ def createindex_stb_crash_anr_v2(es):
     es.indices.create(index = index_name,body=index_settings)
     return
     
-def insert_into_es(es,outputstb,outputdatetime,outputevent,outputver,outeventdes,outprocessname,outcrashtype,outprocessinfo,outanr_process_name,outPID,outFlags,outPackage,outForeground,outExecuting,outBuild):
+def insert_into_es(es,outputstb,outputdatetime,outputevent,outputver,outeventdes,outprocessname,outcrashtype,outprocessinfo,outanr_process_name,outPID,outFlags,outPackage,outForeground,outExecuting,outBuild,outdev_free_mem, outdev_tot_mem):
     #print("outputdatetime=%s" %(outputdatetime))
     writedetstr = {
     "STBID" :outputstb,
@@ -150,9 +157,12 @@ def insert_into_es(es,outputstb,outputdatetime,outputevent,outputver,outeventdes
      "PACKAGE" :outPackage,
      "FOREGROUND" :outForeground,
      "SUBJECT" :outExecuting,
-     "BUILD" :outBuild
+     "BUILD" :outBuild,
+     "DEV_TOT_MEM" :outdev_tot_mem,
+     "DEV_FREE_MEM" :outdev_free_mem
      }
     es.index(index=index_name ,doc_type='_doc', body=writedetstr)
+    #exit(-1)
     return
 
 ## Main Program Starts from here  
@@ -180,10 +190,12 @@ from elasticsearch import Elasticsearch
 #exit(-1)
 
 #These parameters will be parmetrized after development over
-stb,xdatetime,event,version,jsonstr = Read_Five_Column_File('stbdata_V2_7.csv') #Parameterize later
-outcsv = open("crashlog_V2_7.csv","w")  #parameterize
+#stbdata_V2_8 is Mi1 (device memory processing) received on 29-Jun-2020
+#stb,xdatetime,event,version,jsonstr,memstr = Read_Six_Column_File('stbdata_V2_8.csv') #Parameterize later
+jsonstr,version,stb,xdatetime,memstr,event = Read_Six_Column_File('stbdata_V2_8.csv') #Parameterize later
+outcsv = open("crashlog_V2_8.csv","w")  #parameterize
 write_toES = 1 #Parameterize, Is writing to ES required, 1 means Yes
-index_name = 'stb_crash_anr_v2_7' #Parameterize, this is the ES index data goes into
+index_name = 'stb_crash_anr_v2_8' #Parameterize, this is the ES index data goes into
 
 
 if (write_toES == 1): # Check if ES is running and need to create index
@@ -192,7 +204,7 @@ i=0
 eventcnt=0
 failcnt=0
 recordsuccess=0 #0 is succesful
-outcsv.write("STBID,EventDateTime,Event,App_Ver,EventType,Process_name,Process_Info,Crash_Header,Anr_Process_Name,Pid,Flags,Package,Foreground,Subject,Build\n")
+outcsv.write("STBID,EventDateTime,Event,App_Ver,EventType,Process_name,Process_Info,Crash_Header,Anr_Process_Name,Pid,Flags,Package,Foreground,Subject,Build,dev_free_mem,dev_tot_mem\n")
 
 
 while (i<len(stb)):
@@ -204,7 +216,14 @@ while (i<len(stb)):
         
     #Start Processinng XPSE9 & XPSE10 events
     outputstb = stb[i]
+    #print("xdatetime[%d]=" %(i))
+    #print(xdatetime[i])
+    #print(memstr[i])
     outputdatetime = (datetime.strptime(xdatetime[i],'%d-%m-%Y %H:%M:%S')-timedelta(hours=5, minutes = 30)).strftime('%d-%m-%Y %H:%M:%S') #Subtract 5.30 hours, as ES is adding 5.30 hours
+   
+    #print("outputdatetime=%s" %(outputdatetime))
+    #exit(-1)
+    #outputdatetime = (datetime.strptime(xdatetime[i],'%D-%M-%Y %H:%M:%S')-timedelta(hours=5, minutes = 30)).strftime('%D-%M-%Y %H:%M:%S') #Subtract 5.30 hours, as ES is adding 5.30 hours
     #outputdatetime = xdatetime[i]
     outputevent = event[i].strip()
     outputver = version[i]
@@ -212,6 +231,7 @@ while (i<len(stb)):
     jsonstr[i] = jsonstr[i].strip()
     jsonstr[i] = jsonstr[i][1:]
     jsonstr[i] = jsonstr[i][:-1]
+   
     outeventdes = 'NA'
     outprocessname = 'NA'
     outprocessinfo = 'NA'
@@ -223,6 +243,14 @@ while (i<len(stb)):
     outForeground = 'NA'
     outExecuting = 'NA' #name it as outSubject for uniformity?
     outBuild = 'NA'
+    outdev_free_mem = 0
+    outdev_tot_mem = 0
+    #Get Memory data
+    tempmemstr = memstr[i].replace('[','').replace(']','').replace('"','')
+    outdev_free_mem = float(tempmemstr[0:tempmemstr.find(',')])
+    outdev_tot_mem = float(tempmemstr[tempmemstr.find(',')+1:])
+    #print("free=%f,tot=%f"%(outdev_free_mem,outdev_tot_mem))
+    
     data = json.loads(jsonstr[i])
     #If no of ifs, events increase, think of defining a function
     if (outputevent == "XPSE9"):
@@ -248,10 +276,10 @@ while (i<len(stb)):
         
     #Write Output
     if (recordsuccess == 0):
-        outstr ="%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n" %(outputstb,outputdatetime,outputevent,outputver,outeventdes,outprocessname.replace(',','!').replace('\n','@'),outprocessinfo.replace(',','!').replace('\n','@'),outcrashtype.replace(',','!').replace('\n','@'),outanr_process_name,outPID,outFlags,outPackage,outForeground,outExecuting,outBuild) #Replace Required only for csv, while inserting into ES, will remove
+        outstr ="%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%f,%f\n" %(outputstb,outputdatetime,outputevent,outputver,outeventdes,outprocessname.replace(',','!').replace('\n','@'),outprocessinfo.replace(',','!').replace('\n','@'),outcrashtype.replace(',','!').replace('\n','@'),outanr_process_name,outPID,outFlags,outPackage,outForeground,outExecuting,outBuild,outdev_free_mem,outdev_tot_mem) #Replace Required only for csv, while inserting into ES, will remove
         outcsv.write(outstr)
         if (write_toES == 1): #Write record to Elastic Search
-            insert_into_es(esind,outputstb,outputdatetime,outputevent,outputver,outeventdes,outprocessname,outcrashtype,outprocessinfo,outanr_process_name,outPID,outFlags,outPackage,outForeground,outExecuting,outBuild)
+            insert_into_es(esind,outputstb,outputdatetime,outputevent,outputver,outeventdes,outprocessname,outcrashtype,outprocessinfo,outanr_process_name,outPID,outFlags,outPackage,outForeground,outExecuting,outBuild,outdev_free_mem,outdev_tot_mem)
     else: #Reset Recordstatus to success for next record
         recordsuccess = 0
     #Increment, counters as required
