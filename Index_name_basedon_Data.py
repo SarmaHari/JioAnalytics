c47f2b3b7e4d34b6f99154ea8c79c44bbcb306fc
SarmaHari <59437575+SarmaHari@users.noreply.github.com>
2020-08-05 12:14:04 +0530

    Add files via upload
    Added code to remove duplicates

--- Index_name_basedon_Data.py
+++ Index_name_basedon_Data.py
@@ -1,3 +1,4 @@
+#04-05 Aug 2020 Data template changed so change program
 #Arrive at index name based on data
 
 import pandas as pd
@@ -5,17 +6,29 @@ import datetime
 from datetime import datetime
 
 print("Started at " , (datetime.now()))
-input_data = pd.read_excel('STB_ANR_CR_DATA_25000.xlsx')
+input_data = pd.read_csv('XPSE9nXPSE10_30_July_20-Part-1.csv')
+#input_data = pd.read_csv('Input.csv')
 print("Excel Read")
-input_col_Required = input_data[['Event Date']]
+#input_col_Required = input_data[['dc_stb_event_po.Event Date']]
+#30-Jul-2020 data sent on 4-Aug-2020 has a different column Name
+input_col_Required = input_data[['dc_stb_event_po.event_date']]
 print("Required Column Extracted. Number of rows=%d" %(input_col_Required.count()))
-input_col_Required.sort_values('Event Date',inplace = True)
+input_col_Required.sort_values('dc_stb_event_po.event_date',inplace = True)
 print("Sorted Now Rows=%d" %(input_col_Required.count()))
-#input_col_Required.drop_duplicates(subset = 'Event Date',keep = first, inplace = True)
-input_col_Required.drop_duplicates(subset = 'Event Date', inplace = True)
+input_col_Required.drop_duplicates(subset = 'dc_stb_event_po.event_date', inplace = True)
 print("Number of rows after dropping duplicates:%d" %(input_col_Required.count()))
-print(input_col_Required['Event Date'].iloc[0].replace('T00:00:00',''))
-index_name = "jiostb_"+input_col_Required['Event Date'].iloc[0].replace('T00:00:00','')
+print(input_col_Required['dc_stb_event_po.event_date'].iloc[0].replace('T00:00:00',''))
+index_name = "jiostb_"+input_col_Required['dc_stb_event_po.event_date'].iloc[0].replace('T00:00:00','')
 print("Index_Name = %s" %(index_name))
-print("Completed at " , (datetime.now()))
-#print(input_col_Required)
+print("Completed at finding index name" , (datetime.now()))
+#04-Aug-2020, Extract required columns
+input_data = pd.read_csv('XPSE9nXPSE10_30_July_20-Part-1.csv')
+#input_data = pd.read_csv('Input.csv')
+input_cols_Required = input_data[['dc_stb_event_po.as5','dc_stb_event_po.as7','dc_stb_event_po.di8','dc_stb_event_po.dt_raw','dc_stb_event_po.mi1','dc_stb_event_po.name']]
+input_cols_Required .to_csv('Before_Removing_Duplicates.csv',index = False)
+print("Completed Selecting Required Columns" , (datetime.now()))
+input_cols_Required.sort_values(['dc_stb_event_po.di8','dc_stb_event_po.dt_raw','dc_stb_event_po.name','dc_stb_event_po.as5'],inplace = True)
+input_cols_Required .to_csv('After_Sorting.csv',index = False)
+input_cols_Required2 = input_cols_Required.drop_duplicates(['dc_stb_event_po.di8','dc_stb_event_po.dt_raw','dc_stb_event_po.name','dc_stb_event_po.as5'])
+input_cols_Required2 .to_csv('After_Removing_Duplicates.csv',index = False, sep = ',')
+print("Removed Duplicate COlumns" , (datetime.now()))
